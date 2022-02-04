9ba1b36c2a1abd60927ac38e834911aff9e12997
Mahmudul Hasan Hasib <hasib.mahmudul@gmail.com>
2022-02-04 22:53:46 +0600

    wip: setup the layouts

---
+++ vue/src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Surveys from "../views/Surveys.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/dashboard",
+    name: "Dashboard",
+    component: DefaultLayout,
+    children: [
+      {
+        path: "/dashboard",
+        name: "Dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "/surveys",
+        name: "Surveys",
+        component: Surveys,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
